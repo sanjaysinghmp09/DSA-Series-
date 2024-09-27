a0b2fa09a5b1d6ed869c24a4c71dceefae534b0d
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2024-09-27 15:12:09 +0530

    Update countingContinue.cpp

--- countingContinue.cpp
+++ countingContinue.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
 
-int main () {
-    int n = 4 ;
-    
+int main() {
+    int n = 3;  // size of the square
+    int num = 1;  // start counting from 1
 
-    for (int i = 0 ; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << n << " " ;
-            n ++ ;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << num << " ";
+            num++;
         }
-        
+        cout << endl;  // move to the next line after each row
     }
 
-    return 0;
-
-    
+    return 0;  // return after all rows are printed
 }
